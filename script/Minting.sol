f3c5ba800d734df7c6b6a66bdd0eb167cf5e3629
Jake <jh0231015@otc.edu>
2025-01-15 22:55:10 -0600

    Added keys for testing and env

---
+++ script/Minting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+
+contract Minting is Script {
+    function run() external {
+        //this was just to ensure I could link rpc-url and private-key 
+        //will code this soon 
+    }
+}
